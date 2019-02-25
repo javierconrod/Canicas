808f85954a300a5dd692e19c24c65b518d0d1417
Javier Contreras <javierin_kl@hotmail.com>
2019-02-24 21:51:44 -0700

    Initial commit

---
+++ Assets/Poderoso.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Poderoso : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    public int poder = 500;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.AddForce(Vector2.right*poder);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
