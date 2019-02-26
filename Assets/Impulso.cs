ea95281ea9076b84296680dc34c274a44936022e
Javier Contreras <javierin_kl@hotmail.com>
2019-02-25 21:38:08 -0700

    Addforce Impulso

---
+++ Assets/Impulso.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Impulso : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    public float potencia = 300;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.AddForce(new Vector2(potencia,0), ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
